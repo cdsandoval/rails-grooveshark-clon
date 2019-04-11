66f5be62f8fafcabe2478b98372b44e739a2acf9
Brayan Ciudad <linzeur@gmail.com>
2019-04-11 14:38:37 -0500

    Create project, structure folders  and run commands

---
+++ db/migrate/20190411192512_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.integer :duration
+      t.integer :rating
+      t.integer :progress
+
+      t.timestamps
+    end
+  end
+end
