f8a1b0b0e6d152abaad97ca4138d814ad9b283fa
byteconde <iamdeyvi@gmail.com>
2019-04-11 17:05:06 -0500

    Seed in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+a = Song.create(title:"Mi primera cancion", duration: 300, rating:1, progress: 0)
+b = Album.create(title: "Mi primer album", rating: 1)
+c = Artist.create(name: "", age: "29")
+
+Album.first.songs << a
+Album.first.songs 
+
