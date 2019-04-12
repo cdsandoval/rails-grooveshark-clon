e15aa9aa30365aaff78adb852f755d2fcaef121a
byteconde <iamdeyvi@gmail.com>
2019-04-12 15:23:21 -0500

    Show album by id

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -4,4 +4,8 @@ class Api::AlbumsController < ApplicationController
     render json: {status: 'SUCCESS', message: 'Loaded albums', data: albums}, status: :ok
   end
 
+  def show
+    album = Album.find(params[:id])
+    render json: {status: 'SUCCESS', message: 'Loaded album', data: album}, status: :ok 
+  end
 end
