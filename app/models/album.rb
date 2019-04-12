457858a6c52c44607cfbf254a9a24c7e46117531
byteconde <iamdeyvi@gmail.com>
2019-04-12 11:44:41 -0500

    Models validations fixed

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,7 +3,8 @@ class Album < ApplicationRecord
   has_and_belongs_to_many :songs, join_table: 'associations'
 
   validates :title, presence: true,
-            length: {   minimum:1, maximum:300 }
-  validates :rating, presence: true,    
-            length: {   minimum:-1, maximum:1   }
+            length:    { minimum:1, maximum:300 }
+  validates :rating, 
+            inclusion: { in: [ 1, 0, -1 ] }
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,8 @@
 class Artist < ApplicationRecord
-  has_and_belongs_to_many :albums, join_table: 'associations'
   has_and_belongs_to_many :songs, join_table: 'associations'
+  has_and_belongs_to_many :albums, join_table: 'associations'
 
+  validates :name, presence: true, length: {in: 1..300}
   
+  validates :age, presence: true, length: {maximum: 2}, format: {with: /0-9/, message: "invalid age"}
 end
