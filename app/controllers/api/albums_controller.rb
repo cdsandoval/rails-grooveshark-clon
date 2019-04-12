0215851911efebb2c30646a370b97865e8587b52
byteconde <iamdeyvi@gmail.com>
2019-04-12 15:29:17 -0500

    Create option added

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -8,4 +8,23 @@ class Api::AlbumsController < ApplicationController
     album = Album.find(params[:id])
     render json: {status: 'SUCCESS', message: 'Loaded album', data: album}, status: :ok 
   end
+
+  def create 
+    album = Album.new(album_params)
+    if article.save
+      render json: {status: 'SUCCESS', message: 'Saved album', data: album},
+      status: :ok
+    else
+      render json: {status: 'ERROR', message: 'Album not saved', data: album.errors},
+      status: :unprocessable_entity
+    end
+
+  end
+
+
+  private
+
+  def album_params
+    params.permit(:title, :rating)
+  end
 end
