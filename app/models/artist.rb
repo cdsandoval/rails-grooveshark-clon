d0dddffdf1b28c7f7ba796a716c4ccca47dd3b6a
MariethPerez <47047276+MariethPerez@users.noreply.github.com>
2019-04-13 23:34:50 -0500

    Add API Artist (#8)
    * Create project, structure folders  and run commands
    
    * Add files for controllers and new routes
    
    * Add validations
    
    * add validations
    
    * Add methods and routes
    
    * Fix validation and add some test cases
    
    * Update song controller
    
    * Seed added for my dudes
    
    * Fix tests cases and actions
    
    * Seed rating fixed
    
    * Add methods albums in Artist
    
    * update controllers
    
    * Add artist/search
    
    * Add admin/controller
    
    * Add songs and albums in show.html.erb

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,7 @@
 class Artist < ApplicationRecord
-  has_and_belongs_to_many :albums, join_table: 'associations'
   has_and_belongs_to_many :songs, join_table: 'associations'
+  has_and_belongs_to_many :albums, join_table: 'associations'
+
+  validates :name, presence: true, length: {in: 1..300}
+  validates :age, presence: true, numericality:true, length: {maximum: 2}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # Data
-s1 = Song.create(title:"Bohemian Rhapsody", duration: 330, rating:1, progress: 0)
-s2 = Song.create(title:"God Save the Queen", duration: 420, rating:1, progress: 0)
-s3 = Song.create(title:"Love of My Life", duration: 300, rating:1, progress: 0)
+s1 = Song.create(title:"Bohemian Rhapsody", duration: 330, rating:0, progress: 0)
+s2 = Song.create(title:"God Save the Queen", duration: 420, rating:0, progress: 0)
+s3 = Song.create(title:"Love of My Life", duration: 300, rating:0, progress: 0)
 s4 = Song.create(title:"Death on Two Legs", duration: 260, rating:0, progress: 0)
 s5 = Song.create(title:"39", duration: 290, rating:0, progress: 0)
-a_night_ophera = Album.create(title: "A Night at the Opera", rating: 1)
+a_night_ophera = Album.create(title: "A Night at the Opera", rating: 0)
 queen = Artist.create(name: "Queen", age: "50")
 # Insertions
 Album.find(a_night_ophera.id).songs << s1
@@ -32,12 +32,12 @@ Artist.find(queen.id).songs << s4
 Artist.find(queen.id).songs << s5
 
 # Data
-s6 = Song.create(title:"Recess", duration: 230, rating:1, progress: 0)
-s7 = Song.create(title:"Try It Out", duration: 330, rating:1, progress: 0)
-s8 = Song.create(title:"Coast is Clear", duration: 370, rating:1, progress: 0)
-s9 = Song.create(title:"Dirty Vibe", duration: 380, rating:1, progress: 0)
-s10 = Song.create(title:"Easy My Mind", duration: 330, rating:1, progress: 0)
-recess = Album.create(title: "Recess", rating: 1)
+s6 = Song.create(title:"Recess", duration: 230, rating:0, progress: 0)
+s7 = Song.create(title:"Try It Out", duration: 330, rating:0, progress: 0)
+s8 = Song.create(title:"Coast is Clear", duration: 370, rating:0, progress: 0)
+s9 = Song.create(title:"Dirty Vibe", duration: 380, rating:0, progress: 0)
+s10 = Song.create(title:"Easy My Mind", duration: 330, rating:0, progress: 0)
+recess = Album.create(title: "Recess", rating: 0)
 skrillex = Artist.create(name: "Skrillex", age: "31")
 # Insertions
 Album.find(recess.id).songs << s6
@@ -65,12 +65,12 @@ Artist.find(skrillex.id).songs << s9
 Artist.find(skrillex.id).songs << s10
 
 # Data
-s11 = Song.create(title:"Ten Seconds Before Sunrise", duration: 380, rating:1, progress: 0)
-s12 = Song.create(title:"Everything", duration: 290, rating:1, progress: 0)
-s13 = Song.create(title:"Dance4life", duration: 230, rating:1, progress: 0)
-s14 = Song.create(title:"Elements Of Life", duration: 310, rating:1, progress: 0)
-s15 = Song.create(title:"Driving to Heaven", duration: 340, rating:1, progress: 0)
-elements_of_life = Album.create(title: "Elements of Life", rating: 1)
+s11 = Song.create(title:"Ten Seconds Before Sunrise", duration: 380, rating:0, progress: 0)
+s12 = Song.create(title:"Everything", duration: 290, rating:0, progress: 0)
+s13 = Song.create(title:"Dance4life", duration: 230, rating:0, progress: 0)
+s14 = Song.create(title:"Elements Of Life", duration: 310, rating:0, progress: 0)
+s15 = Song.create(title:"Driving to Heaven", duration: 340, rating:0, progress: 0)
+elements_of_life = Album.create(title: "Elements of Life", rating: 0)
 tiesto = Artist.create(name: "Tiesto", age: "50")
 # Insertions
 Album.find(elements_of_life.id).songs << s11
@@ -98,12 +98,12 @@ Artist.find(tiesto.id).songs << s14
 Artist.find(tiesto.id).songs << s15
 
 # Data
-s16 = Song.create(title:"Breach (Walk Alone)", duration: 200, rating:1, progress: 0)
-s17 = Song.create(title:"Yottabyte", duration: 210, rating:1, progress: 0)
-s18 = Song.create(title:"Latency", duration: 290, rating:1, progress: 0)
-s19 = Song.create(title:"Access", duration: 140, rating:1, progress: 0)
-s20 = Song.create(title:"Waiting for Tomorrow", duration: 190, rating:1, progress: 0)
-bylaw = Album.create(title: "BYLAW", rating: 1)
+s16 = Song.create(title:"Breach (Walk Alone)", duration: 200, rating:0, progress: 0)
+s17 = Song.create(title:"Yottabyte", duration: 210, rating:0, progress: 0)
+s18 = Song.create(title:"Latency", duration: 290, rating:0, progress: 0)
+s19 = Song.create(title:"Access", duration: 140, rating:0, progress: 0)
+s20 = Song.create(title:"Waiting for Tomorrow", duration: 190, rating:0, progress: 0)
+bylaw = Album.create(title: "BYLAW", rating: 0)
 martin_garrix = Artist.create(name: "Martin Garrix", age: "50")
 # Insertions
 Album.find(bylaw.id).songs << s16
@@ -124,12 +124,12 @@ Artist.find(martin_garrix.id).songs << s19
 Artist.find(martin_garrix.id).songs << s20
 
 # Data
-s21 = Song.create(title:"Shaky Shaky", duration: 240, rating:1, progress: 0)
-s22 = Song.create(title:"Adictiva", duration: 230, rating:1, progress: 0)
-s23 = Song.create(title:"Hula Hoop", duration: 200, rating:1, progress: 0)
-s24 = Song.create(title:"Vuelve", duration: 220, rating:1, progress: 0)
-s25 = Song.create(title:"Con Calma", duration: 230, rating:1, progress: 0)
-el_disco_duro = Album.create(title: "BYLAW", rating: 1)
+s21 = Song.create(title:"Shaky Shaky", duration: 240, rating:0, progress: 0)
+s22 = Song.create(title:"Adictiva", duration: 230, rating:0, progress: 0)
+s23 = Song.create(title:"Hula Hoop", duration: 200, rating:0, progress: 0)
+s24 = Song.create(title:"Vuelve", duration: 220, rating:0, progress: 0)
+s25 = Song.create(title:"Con Calma", duration: 230, rating:0, progress: 0)
+el_disco_duro = Album.create(title: "BYLAW", rating: 0)
 daddy_yankee = Artist.create(name: "Martin Garrix", age: "50")
 # Insertions
 Album.find(el_disco_duro.id).songs << s21
