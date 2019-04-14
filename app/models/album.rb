366b161996e3252703210e4a688137a4f7ad9e03
byteconde <iamdeyvi@gmail.com>
2019-04-13 20:36:25 -0500

    Just a simple space

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ApplicationRecord
   has_and_belongs_to_many :artists, join_table: 'associations'
   has_and_belongs_to_many :songs, join_table: 'associations'
 
-  validates :title, presence: true,
+  validates :title, presence: true ,
             length:    { minimum:1, maximum:300 }
   validates :rating, 
             inclusion: { in: [ 1, 0, -1 ] }
