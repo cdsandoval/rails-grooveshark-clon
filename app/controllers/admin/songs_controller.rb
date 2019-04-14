cf83ec37ac9da6ad0ba3298ed9d6c2cea440eaf1
Cesar Cachay <cesarcachay1996@gmail.com>
2019-04-13 20:48:50 -0500

    Add views in admin/songs

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -1,7 +1,40 @@
-class Admin::SongsController <ApplicationController
-  def destroy
-    song = Song.find(params[:id])
-    song.delete
+class Admin::SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
   end
 
+  def show
+    song = Song.find(params[:id])
+    render json: song
+  end
+  
+  def new
+    @song = Song.new
+  end
+
+  def create  
+    @song = Song.new(song_params)
+    @song.save
+    redirect_to admin_song_path(@song), notice: "The song was created successfully, enjoy!"
+  end
+
+  def edit
+    @song = Song.find(params[:id_song])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(params)
+      redirect_to admin_song_path, notice: "This song was updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    song = Song.find(params[:id])
+    song.destroy
+    redirect_to admin_song_path, notice: "The song was successfully deleted"
+  end
 end
