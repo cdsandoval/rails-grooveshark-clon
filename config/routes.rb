a25798c252f16fd2339a4bafd5f9479c6f066ea0
byteconde <iamdeyvi@gmail.com>
2019-04-13 20:26:20 -0500

    album routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,29 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :albums
-    resources :artists
-    resources :songs
+    resources :artists 
+
+    resources :songs, only: [ :index, :show] do
+      member do
+        get :artists
+        get :albums
+
+        put "/progress" => :song_progress
+        put "/rating" => :song_rating 
+      end
+    end
+
+    resources :albums, only: [ :index, :show] do
+      member do
+        get :songs
+        get :artists
+
+        put "/rating" => :album_rating
+        
+      end
+    end
+
   end
+
 
 end
