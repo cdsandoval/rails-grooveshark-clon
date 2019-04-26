8834946cf3e9b9101dc463d08addac298c80ec25
Valeria Vassallo <valevassallo1@gmail.com>
2019-04-25 19:32:04 -0500

    Add after_sign_in_path_for  method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   def after_sign_in_path_for(resource)
     if current_user.role == "admin"
-      admin_home_index_path
+      admin_artists_path
     elsif current_user.role = "regular"
       "home#index"
     end
