8b5a145d77afdc56e624c306e64bd352b251f563
Ricardo <ry.yrupailla@gmail.com>
2019-04-26 16:50:28 -0500

    Refactor pundit for songs

--- app/policies/admin/song_policy.rb
+++ app/policies/admin/song_policy.rb
@@ -3,5 +3,25 @@ class Admin::SongPolicy < ApplicationPolicy
   def index?  
     @user&.role?('admin')
   end
+
+  def show?  
+    @user&.role?('admin')
+  end
+
+  def new?  
+    @user&.role?('admin')
+  end
+
+  def edit?  
+    @user&.role?('admin')
+  end
+
+  def update?  
+    @user&.role?('admin')
+  end
+
+  def destroy?  
+    @user&.role?('admin')
+  end
  
 end 
--- app/policies/api/song_policy.rb
+++ app/policies/api/song_policy.rb
@@ -3,5 +3,24 @@ class Api::SongPolicy < ApplicationPolicy
   def index?  
     @user
   end
- 
+
+  def show?  
+    @user
+  end
+
+  def search?  
+    @user
+  end
+
+  def artists?  
+    @user
+  end
+
+  def song_progress?  
+    @user
+  end
+
+  def song_rating?  
+    @user
+  end
 end 
