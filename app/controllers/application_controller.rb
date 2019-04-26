10a1433501dd6b8dd96ecc8a6cf930e73e45fe05
Ricardo <ry.yrupailla@gmail.com>
2019-04-26 10:51:47 -0500

    Add  rescue for authorization erro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,25 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def after_sign_in_path_for(resource)
     if current_user.role == "admin"
       admin_artists_path
     elsif current_user.role = "regular"
-      "home#index"
+      root_path
     end
+  end
+
+  def after_sign_up_path_for(resource)
+    if current_user.role == "admin"
+      admin_artists_path
+    elsif current_user.role = "regular"
+      root_path
+    end
+  end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 end
